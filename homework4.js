c558303681899debc33dc08773522b4d149c7220
dimamitrakow630 <dima.mitrakow7@gmail.com>
2026-06-06 14:37:40 +0200

    Create homework4.js

---
+++ homework4.js
@@ -0,0 +1,12 @@
+function checkProbabilityTheory(count){
+    let odds = 0;
+    let evens = 0;
+
+         for (let i = 0; i < count; i++){
+    let number = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+         if(number % 2 === 0) {evens++} else {odds++}
+         }
+
+   return `total numbers: ${odds: ${odds}, evens: ${evens} \n odds probability: ${odds / count} \n evens probability: ${evens / count}
+}
+}
