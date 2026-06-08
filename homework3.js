ba58873eb60fb8b112bce98240d5ec01ee76a27a
dimamitrakow630 <dima.mitrakow7@gmail.com>
2026-06-08 10:10:01 +0200

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -1,10 +1,10 @@
 function pow(a, b) {
     let result = 1;
-
+    if (b < 0) { b = Math.abs(b)};
     for (let i = 0; i < b; i++) {
         result = result * a;
     }
-
+    
     return result; 
 }
 
